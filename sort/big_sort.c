fd3955762b30c08c1ff84db15f8fd863d5f9a221
CrystalSith <joaquim1810@gmail.com>
2024-06-27 11:12:14 +0200

    idea

--- sort/big_sort.c
+++ sort/big_sort.c
@@ -6,7 +6,7 @@
 /*   By: jopfeiff <jopfeiff@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 18:23:38 by jopfeiff          #+#    #+#             */
-/*   Updated: 2024/06/27 09:50:13 by jopfeiff         ###   ########.fr       */
+/*   Updated: 2024/06/27 11:11:05 by jopfeiff         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -166,97 +166,47 @@ static int	rest2(t_node **a, int mid, int quart)
 	return (0);
 }
 
-static int	rest(t_node **a, int quart)
+static int	rest(t_node **a, int quart, int not)
 {
 	t_node	*tmp;
 
 	tmp = *a;
 	while (tmp)
 	{
-		if (tmp->data < quart)
+		if (tmp->data < quart && tmp->data > not)
 			return (1);
 		tmp = tmp->next;
 	}
 	return (0);
 }
-static void	check_sizes(t_node **a, t_node **b)
-{
-	if (!b || !*b)
-		return;
-	if (!a|| !*a)
-		return;
-	updt_list(b);
-	updt_list(a);
-	if ((*a)->data == (*a)->highest)
-		ra(a);
-	if ((*a)->next && (*b)->next)
-	{
-		if ((*b)->data < (*b)->next->data && (*a)->data > (*a)->next->data)
-			ss(a, b);
-	}
-	if ((*a)->next != NULL)
-	{
-		if ((*a)->data > (*a)->next->data)
-			sa(a);
-	}
-	if ((*b)->next != NULL)
-	{
-		if ((*b)->data < (*b)->next->data)
-			sb(b);
-	}
-}
-
-static void	to_bb(t_node **a, t_node **b)
-{
-	int	mid;
-	int	quart;
-	updt_list(a);
-	mid = (*a)->median;
-	quart = mid / 2;
-	while (rest2(a, mid, quart))
-	{
-		updt_list(a);
-		check_sizes(a, b);
-		if ((*a)->data < (mid + quart))
-			pb(a, b);
-		else
-			ra(a);
-	}
-	while (*a)
-	{
-		check_sizes(a, b);
-		pb(a, b);
-	}
-}
-
-
-static void	to_b(t_node **a, t_node **b)
-{
-	int	mid;
-	int	quart;
-	
-	updt_list(a);
-	mid = (*a)->median;
-	quart = mid / 2;
-	while (rest(a, quart))
-	{
-		updt_list(a);
-		check_sizes(a, b);
-		if ((*a)->data < quart)
-			pb(a, b);
-		else
-			ra(a);
-	}
-	while (rest(a, mid))
-	{
-		updt_list(a);
-		check_sizes(a, b);
-		if ((*a)->data < mid)
-			pb(a, b);
-		else
-			ra(a);
-	}
-}
+// static void	check_sizes(t_node **a, t_node **b, int mid, int quart)
+// {
+// 	if (!b || !*b)
+// 		return;
+// 	if (!a|| !*a)
+// 		return;
+// 	updt_list(b);
+// 	updt_list(a);
+// 	if ((*a)->data == (*a)->highest)
+// 		ra(a);
+// 	// if ((*a)->next && (*b)->next)
+// 	// {
+// 	// 	if ((*b)->data < (*b)->next->data && (*a)->data > (*a)->next->data)
+// 	// 		ss(a, b);
+// 	// }
+// 	if ((*a)->next != NULL)
+// 	{
+// 		if ((*a)->data > (*a)->median && (*a)->data < quart + mid)
+// 			ra(a);	
+// 		if ((*a)->data > (*a)->next->data)
+// 			sa(a);
+// 	}
+// 	if ((*b)->next != NULL)
+// 	{
+// 		if ((*b)->data < (*b)->next->data)
+// 				return ;
+// 	}
+// }
 
 static void	to_a(t_node **a, t_node **b)
 {
@@ -280,6 +230,69 @@ static void	to_a(t_node **a, t_node **b)
 		pa(a, b);
 	}
 }
+static void	to_bb(t_node **a, t_node **b)
+{
+	int	mid;
+	int	quart;
+	updt_list(a);
+	mid = (*a)->median;
+	quart = mid / 2;
+	while (rest2(a, mid, quart))
+	{
+		updt_list(a);
+		//check_sizes(a, b, mid, quart);
+		if ((*a)->data < (mid + quart) /*&& (*a)->data > quart*/)
+			pb(a, b);
+		else
+			ra(a);
+	}
+	to_a(a, b);
+	while (*a)
+	{
+		
+		pb(a, b);
+	}
+	to_a(a, b);
+}
+// static void	check_last(t_node **a)
+// {
+// 	t_node	*tmp;
+
+// 	tmp = *a;
+// 	while (tmp->next)
+// 		tmp = tmp->next;
+// 	if (tmp->data < tmp->median)
+// 		rra(a);
+// }
+
+static void	to_b(t_node **a, t_node **b)
+{
+	int	mid;
+	int	quart;
+	
+	updt_list(a);
+	mid = (*a)->median;
+	quart = mid / 2;
+	while (rest(a, quart, 0))
+	{
+		updt_list(a);
+		if ((*a)->data < quart)
+			pb(a, b);
+		else
+			ra(a);
+	}
+	to_a(a, b);
+	while (rest(a, mid, quart))
+	{
+		updt_list(a);
+		if ((*a)->data < mid && (*a)->data > quart)
+			pb(a, b);
+		else
+			ra(a);
+	}
+	to_a(a, b);
+}
+
 
 void	big_sort(t_node **a, t_node **b)
 {
@@ -289,6 +302,5 @@ void	big_sort(t_node **a, t_node **b)
 	{
 		to_b(a, b);
 		to_bb(a, b);
-		to_a(a, b);
  	}
 }
